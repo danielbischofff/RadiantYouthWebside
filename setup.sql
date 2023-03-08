6bc1c24ff8ad2cb360096d80d03f7448ff84c244
danielbischofff <d.bischoff@student.uni-tuebingen.de>
2023-03-08 22:58:03 +0100

    update myswl file #9

--- setup.sql
+++ setup.sql
@@ -1,26 +1,34 @@
 CREATE DATABASE SummerSplash;
 USE SummerSplash;
-CREATE TABLE SummerSplashAnmeldungen2023 (
-    id INT NOT NULL, -- automatisch generierte ID
-    parentsMail TEXT NOT NULL, -- Email Adresse der Teilnehmer oder Erziehungsberechtigter
-    mailParticipant TEXT NOT NULL, -- Mail d.T
+CREATE TABLE Teilnehmer2023 (
+    -- ALLGEMEINE ANGABEN
+    id INT NOT NULL AUTO_INCREMENT, 
     forname TEXT NOT NULL, -- Vorname des Teilnehmers
     surname TEXT NOT NULL, -- Nachname des Teilnehmers
     sex TEXT NOT NULL, -- Geschlecht des Teilnehmers
-    birthday DATE NOT NULL, -- Geburtstag d.T.
-    adress TEXT NOT NULL, -- Adresse d.T in der Form Straße Hausnummer, PZL, Stadtname
-    emergencyContact TEXT NOT NULL, -- Notfallkontakt des Teilnehmers
     church TEXT NOT NULL, -- Heimgemeinde
+    birthday TEXT NOT NULL, -- Geburtstag d.T.  Datenformat verbessern
+    birthdayCountry TEXT NOT NULL, -- Geburtsland des d.T
+    -- adress TEXT NOT NULL, -- Adresse d.T in der Form Straße Hausnummer, PZL, Stadt
+    street_number TEXT NOT NULL,
+    postalCode TEXT NOT NULL,
+    city TEXT NOT NULL,
+    -- Kontaktmöglichkeiten
+    mailParents TEXT NOT NULL, -- Email Adresse der Teilnehmer oder Erziehungsberechtigter
+    mailParticipant TEXT NOT NULL, -- Mail d.T
+    -- wichtige Informationen
+    emergencyContact TEXT NOT NULL, -- Notfallkontakt des Teilnehmers
     eatingHabits TEXT NOT NULL, -- Unverträglichkeiten, Vegetarisch, Vegan, Sonstiges
     medicalNote TEXT NOT NULL, -- Krankheiten, Medikamente
-    swimmPermission BIT NOT NULL, -- Schwimmerlaubnis des d.T
-    roomRequest TEXT NOT NULL, -- Zimmerwunsch
+    -- zusätzliche Informationen
+    swimmPermission TEXT NOT NULL, -- Schwimmerlaubnis des d.T   
+    roomRequest TEXT NOT NULL, -- Zimmerwunsch 
     dutyArea TEXT NOT NULL, -- Dienstbereich d.T.
     workshop TEXT NOT NULL, -- Workshopwünsche der Teilnehmer
-    rulesApproval TEXT NOT NULL, -- Zustimmung der AGB's und der Summersplash Regeln
-    tshirt BIT NOT NULL, -- SplashShirt 
-    tshirtSize TEXT NOT NULL, -- T-shirt größe
-    siblings BIT NOT NULL, -- Geschwister auf dem Summersplash
-    siblingsPerson TEXT NOT NULL, -- Nenne der Geschwister auf dem Summersplash
+    rulesApproval TEXT NOT NULL, -- Zustimmung der AGB's und der Summersplash Regeln   
+    tshirt TEXT NOT NULL, -- SplashShirt JA/NEIN 
+    tshirtSize TEXT , -- T-shirt Größe
+    siblings INT NOT NULL, -- Geschwisteranzahl auf dem Summersplash 
+    siblingsPerson TEXT NOT NULL, -- Geschwister auf dem Summersplash Rückgabewert "geschwister 1__geschwister 2" getrennt mit Underscore
     PRIMARY KEY (id)
 );
